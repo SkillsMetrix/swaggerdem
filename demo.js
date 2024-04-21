24478e7956298365f66abf44fb87fd2160ef1656
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-04-21 15:42:59 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -60,3 +60,60 @@ paths:
   /orders:
     summary: to access orders
 
+
+
+--------------------------
+  step-3
+
+openapi: 3.0.3
+info:
+  title: Swagger ONline Store - OpenAPI 3.0
+  version: 1.0..0
+  description: This is a online store to buy a product
+  license:
+    name: Apache 2.0
+    url: http://www.apache.org/licenses/LICENSE-2.0.html
+servers:
+  - url: http://onlinestore.com/api
+    variables: {}
+    description: URL for the API
+tags:
+  - name: products
+    description: access to product
+  - name: orders
+    description: for order app
+paths:
+  /products:
+    summary: access to product apis
+    get:
+      summary: get all products
+      description: ''
+      operationId: getProducts
+      responses:
+        '200':
+          description: List of products
+          content:
+            application/json:
+              schema:
+                type: array
+                items:
+                  $ref: '#/components/schema/product'
+      tags:
+        - products
+  /orders:
+    summary: to access orders
+    post:
+      summary: place a new order
+      description: ''
+      operationId: createOrder
+      responses:
+        '201':
+          description: order created
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schema/product'
+      tags:
+        - orders
+
+
